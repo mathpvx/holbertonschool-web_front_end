01c011af4fcf5e8dd796ebd5c588ec76892895ce
mathpvx <7547@holbertonstudents.com>
2024-11-28 13:25:53 +0100

    basic synchronous functions

---
+++ Javascript_advanced/8-payments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+function processPayment(amount) {
+	console.log(`Collecting payment of ${amount}`)
+}
+function processOrder(orderId, amount) {
+	console.log(`${orderId} is being processed`)
+	processPayment(amount);
+	console.log(`${orderId} has been fully processed`)
+}
+
+console.log("Processing orders")
+processOrder(12321, 10.99);
+processOrder(12322, 12.99);
+processOrder(12323, 15.0);
+console.log("All the orders have been processed");
